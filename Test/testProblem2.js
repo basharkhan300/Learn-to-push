11a012000d9a8fb658804e2056ebcc4a995a59cc
Bashar <bashar.khan.17.1@mountblue.tech>
2021-08-25 17:45:33 +0530

    Details of last car

---
+++ Test/testProblem2.js
@@ -0,0 +1,4 @@
+const problem2 = require('../problem2');
+const inventoryData = require('../inventoryData');
+
+problem2(inventoryData);
---
+++ problem2.js
@@ -0,0 +1,11 @@
+
+function problem2(inventory){
+    console.log(inventory[inventory.length - 1]);
+} 
+
+module.exports = problem2;
+
+
+// let lastCar = result2(inventory);
+
+// console.log( "Last car is a " + lastCar["car_make"] + " " + lastCar["car_model"]);
